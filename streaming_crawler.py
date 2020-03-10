a02a6ae982c4d63cc4246e3df1da725951bb0d03
Leonore Papaloizos <2264897V@student.gla.ac.uk>
2020-03-10 16:05:12 +0000

    adjustments to streaming crawler

--- streaming_crawler.py
+++ streaming_crawler.py
@@ -10,20 +10,23 @@ ACCESS_SECRET = access_secret
 
 client = MongoClient()
 db = client.twitter_db
-collection = "streaming_crawler_1a"
+collection = "streaming_crawler_1003_filtered"
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
 api = tweepy.API(auth, parser=tweepy.parsers.JSONParser(), wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
+count = 0
+
 class TwitterStream(tweepy.StreamListener):
 
     def on_status(self, status):
+        global count
         json_tweet = status._json
         # convert datetime
-        print(json_tweet['text'])
         json_tweet['created_at'] = datetime.strptime(json_tweet['created_at'], '%a %b %d %H:%M:%S +0000 %Y')
         db[collection].insert(json_tweet)
+        count += 1
         return True
 
     def on_error(self, status_code):
@@ -38,12 +41,14 @@ start = datetime.now()
 one_hour = start + timedelta(minutes=60)
 
 streamer.sample(languages=["en"], is_async=True)
+#streamer.filter(track=["coronavirus", "covid-19", "covid19", "SARS-COV-2", "SARS-COV2", "2019-nCov", "covid", "cov19"],
+                languages=["en"], is_async=True)
 
 while datetime.now() < one_hour:
     continue
 
 streamer.disconnect()
-print("Tweets were streamed from {} to {}".format(start, one_hour))
+print("{} tweets were streamed from {} to {} in {}".format(count, start, one_hour, collection))
 
 # Tweets were streamed from 2020-02-16 19:20:48.656979 to 2020-02-16 20:20:48.656979
 # Count: 53124
