bc5a6998c0bc64e02b9b67055b70059922df13c1
Leonore Papaloizos <2264897V@student.gla.ac.uk>
2020-03-14 15:58:44 +0000

    clean hybrid_crawler

--- hybrid_crawler.py
+++ hybrid_crawler.py
@@ -2,43 +2,50 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Crawl tweets for one hour and store in a MongoDB.')
 
-parser.add_argument('--database', '-db', action='store', type=str, help='Name of the collection to store the data in', required=True)
+parser.add_argument('--collection', '-c', action='store', type=str, help='Name of the collection to store the data in (in MongoDB.twitter_db)', required=True)
 parser.add_argument('--time', '-t', action="store", type=int, help="How long to run the crawler for in minutes", required=False)
+parser.add_argument('--verbose', '-v', action="store_true", help="Print remaining requests left", required=False) # verbose prints number of requests left in time window
 
 args = parser.parse_args()
-collection = args.database
+collection = args.collection
 duration = args.time
 if not duration:
     duration = 60
 
-# do imports after argparse
-# imports take time; arguments might be faulty
-import tweepy
+## do imports after argparse for performance
+
+
 from datetime import datetime, timedelta
+import threading
 import time
-from pymongo import MongoClient
-from pymongo.errors import DuplicateKeyError
-from keys import consumer_key, consumer_secret, access_token, access_secret
 import random
 
-import threading
+import tweepy
+from pymongo import MongoClient
+from pymongo.errors import DuplicateKeyError
+
+from keys import consumer_key, consumer_secret, access_token, access_secret
 from helpers import SetQueue
 
+# set up MongoDB connection
+client = MongoClient()
+db = client.twitter_db
+
+# set up Twitter API
 CONSUMER_KEY = consumer_key
 CONSUMER_SECRET = consumer_secret
 ACCESS_TOKEN = access_token
 ACCESS_SECRET = access_secret
 
-client = MongoClient()
-db = client.twitter_db
-
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
+# keep track of Tweet and number of duplicates
 count = 0
 duplicates = 0
 
+# location coordinates for geosearch REST probe
 LONDON = "51.5287352,-0.3817825,100km"
 GENEVA = "46.204391,6.143158,100km"
 WASHINGTON_DC = "38.907192,-77.036873,100km"
@@ -60,7 +67,7 @@ def add_to_database(status):
     count += 1
 
 def process_network(user):
-    # user needs to not have 0 following
+    # user needs to not have suspicious following
     # if user has > 15k followers --> get friends and add to queue
     # if user has > 15k followers --> get replies
     global user_queue
@@ -92,7 +99,6 @@ def parse_for_entities(status):
     global hashtag_queue, user_queue
     user = status.user.id
     user_queue.put(user)
-    # TODO change with get body
     for hashtag in status.entities['hashtags']:
         # add hashtag to list if not there
         hashtag_queue.put(hashtag['text'].lower())
@@ -108,7 +114,7 @@ def hashtag_thread():
 
 def location_thread():
     print("Location thread started.")
-    location = random.choice(locations)
+    location = random.choice(locations) # each time pick a random location to do search for
     while not stop:
         if available_requests['search'] != 0:
             for tweet in tweepy.Cursor(api.search, q="covid", geocode=location, lang="en", count=100, tweet_mode='extended').items():
@@ -148,7 +154,7 @@ time_limit = start + timedelta(minutes=duration)
 stop = False
 
 streamer.filter(track=["coronavirus", "covid-19", "covid19", "SARS-COV-2", "SARS-COV2", "2019-nCov", "covid", "cov19", "SARSCov2"],
-                languages=["en"], is_async=True)
+                languages=["en"], is_async=True) # change this for different (or no) keywords
 
 available_requests = {
     'user_timeline': 0,
@@ -171,24 +177,25 @@ except Exception as e:
 print("Looking for tweets until {}\n".format(time_limit))
 
 while datetime.now() < time_limit:
-    limits = api.rate_limit_status()
+    if args.verbose:
+        limits = api.rate_limit_status()
 
-    rate = limits['resources']['application']['/application/rate_limit_status']
-    print("GET RATES: {} requests left".format(rate['remaining']))
+        rate = limits['resources']['application']['/application/rate_limit_status']
+        print("GET RATES: {} requests left".format(rate['remaining']))
 
-    tl_tweets = limits['resources']['statuses']['/statuses/user_timeline']
-    print("GET TIMELINE: {} requests left".format(tl_tweets['remaining']))
-    available_requests['user_timeline'] = tl_tweets['remaining']
+        tl_tweets = limits['resources']['statuses']['/statuses/user_timeline']
+        print("GET TIMELINE: {} requests left".format(tl_tweets['remaining']))
+        available_requests['user_timeline'] = tl_tweets['remaining']
 
-    followers = limits['resources']['followers']['/followers/ids']
-    print("GET FOLLOWERS: {} requests left".format(followers['remaining']))
-    available_requests['followers'] = followers['remaining']
+        followers = limits['resources']['followers']['/followers/ids']
+        print("GET FOLLOWERS: {} requests left".format(followers['remaining']))
+        available_requests['followers'] = followers['remaining']
 
-    search = limits['resources']['search']['/search/tweets']
-    print("SEARCH TWEETS: {} requests left\n".format(search['remaining']))
-    available_requests['search'] = search['remaining']
+        search = limits['resources']['search']['/search/tweets']
+        print("SEARCH TWEETS: {} requests left\n".format(search['remaining']))
+        available_requests['search'] = search['remaining']
 
-    time.sleep(30)
+    time.sleep(30) # don't run into limits for asking for number of requests left
 
 stop = True
 
